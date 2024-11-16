6a49c39ac38240bdddbdd51f3d4204e09dac4ee9
jeyenlam <yenlam.roxal@gmail.com>
2024-11-15 23:16:25 -0500

    basic active navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import About from "@/components/About";
-import ContactBar from "@/components/ContactBar";
 import Nav from "@/components/Nav";
-import InfoBar from "@/components/InfoBar";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="flex flex-col overflow-x-hidden">
-      <div className="lg:fixed">
+      {/* <div className="lg:fixed">
         <Nav />
-        <InfoBar />
-        <ContactBar />
-      </div>
-      <div className="flex flex-col justify-center p-10 md:p-20 lg:ml-[30rem] md:mr-[7rem]">
+      </div> */}
+      <Nav/>
+      {/* <div className="flex flex-col justify-center gap-20 lg:gap-0 p-10 md:p-20 lg:ml-[30rem] md:mr-[7rem]">
         <About />
-      </div>
+        <Projects />
+      </div> */}
     </main>
   );
 }
